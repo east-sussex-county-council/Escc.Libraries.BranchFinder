91d55c9406d1d0c8e491d2d3af61e7a07d81d629
Rick Mason <richard.mason@eastsussex.gov.uk>
2015-03-30 17:52:25 +0100

    Replace postcode lookup with fake for testing

---
+++ Escc.Libraries.BranchFinder.Website/FakePostcodeLookup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Escc.Geo;
+
+namespace Escc.Libraries.BranchFinder.Website
+{
+    /// <summary>
+    /// A fake postcode lookup implementation which always returns the coordinates of County Hall
+    /// </summary>
+    public class FakePostcodeLookup : IPostcodeLookup
+    {
+        public LatitudeLongitude CoordinatesAtCentreOfPostcode(string postcode)
+        {
+            return new LatitudeLongitude(50.872066, 0.0010903126);
+        }
+    }
+}

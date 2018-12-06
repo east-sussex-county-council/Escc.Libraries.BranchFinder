616f149871425deceb3caebe85939832dd4a7a92
Rick Mason <richard.mason@eastsussex.gov.uk>
2018-12-06 17:26:48 +0000

    The redirect went on the return the view, which then threw an exception because headers had already been sent. Redirect differently to fix it.

--- Escc.Libraries.BranchFinder.Website/DefaultController.cs
+++ Escc.Libraries.BranchFinder.Website/DefaultController.cs
@@ -1,5 +1,4 @@
-﻿using Escc.Web;
-using System;
+﻿using System;
 using System.IO;
 using System.Web.Mvc;
 
@@ -13,7 +12,7 @@ namespace Escc.Libraries.BranchFinder.Website
             // Ensure there's one version of this URL so that the data is consistent in Google Analytics
             if (Path.GetFileName(Request.RawUrl).ToUpperInvariant() == "DEFAULT.ASPX")
             {
-                new HttpStatus().MovedPermanently(Url.Content("~/"));
+                return new RedirectResult(Url.Content("~/"), true);
             }
 
             return View(new LibrariesViewModel());
